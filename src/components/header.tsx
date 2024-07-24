6706ad0e5cbb773573ce43cd794eb9c7a5ea817f
Manjot <mssond459@gmail.com>
2024-07-24 22:49:08 +0530

    shadcn is added

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,7 @@
 'client'
 import React from 'react'
 import Image from 'next/image';
+import { Button } from "@/components/ui/button"
 
 export default function Header() {
   return(
@@ -8,6 +9,7 @@ export default function Header() {
       <Image src="/logo.png" alt="Logo" width={80} height={80} />
       <p>Deepak Verma</p>
 
+      <Button>Click me</Button>
     </div>
   );
   
