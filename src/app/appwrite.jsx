60883d6d9945d121abecb5e2b1ec503673c1996b
manjot <mssond459@gmail.com>
2024-07-28 02:43:19 +0530

    appwrite config is added

--- src/app/appwrite.jsx
+++ src/app/appwrite.jsx
@@ -1,8 +1,8 @@
 import { Client, Account } from 'appwrite';
 export const client = new Client();
 client
-    .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject('66a50a6c000f1db2591b');
+    .setEndpoint(process.env.APPWRITE_ENDPOINT)
+    .setProject(process.env.APPWRITE_ID);
 
 export const account = new Account(client);
 export { ID } from 'appwrite';
---
+++ src/app/login/page.tsx
@@ -0,0 +1,52 @@
+"use client";
+import React, { useState } from "react";
+import { Client, Account } from "appwrite";
+
+const Login: React.FC = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  return (
+    <div className="flex p-4 space-x-5">
+      <input
+        className="text-black"
+        type="text"
+        value={email}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setEmail(e.target.value)
+        }
+        placeholder="Email"
+      />
+      <input
+        className="text-black"
+        type={showPassword ? "text" : "password"}
+        value={password}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setPassword(e.target.value)
+        }
+        placeholder="Password"
+      />
+      <button
+        onClick={() => {
+          if(showPassword === false){
+            setShowPassword(true);
+          } else {
+            setShowPassword(false);
+          }
+        }}
+      >
+        show password
+      </button>
+      <button
+        onClick={() => {
+          console.log(email);
+        }}
+      >
+        show
+      </button>
+    </div>
+  );
+};
+
+export default Login;
